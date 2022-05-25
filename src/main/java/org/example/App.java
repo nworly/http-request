88ae5e7ccffea4b047e30851d60641371196bbc5
Elizaveta Zabolotnaia <zabolotnaya.ea@gmail.com>
2022-05-25 22:31:59 +0300

    init commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class App {
+    public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My Test Service")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+//        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+        List<Fact> facts = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
+        });
+
+        facts.stream()
+                .filter(fact -> fact.getUpvotes() != null)
+                .forEach(System.out::println);
+    }
+}
